bf9709870fdbdddcdad3f9406023e72935d27342
PIYUS1507 <piyushrajurkar205@gmail.com>
2026-08-11 17:49:30 +0530

    production related work

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,26 @@ import dotenv from "dotenv"
 import express from "express"
 import authRoute from "./routes/auth.routes.js"
 import messageRoute from "./routes/message.routes.js"
-
+import path from "path"
 
 dotenv.config()
 const app = express()
+
+const __dirname = path.resolve()
 
 app.use("/api/auth",authRoute)
 app.use("/api/message",messageRoute)
 
 
 // console.log(process.env.PORT)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log(`the server is listening on the port ${process.env.PORT || 3000}`)
